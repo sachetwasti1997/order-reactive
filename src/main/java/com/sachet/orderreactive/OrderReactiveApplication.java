b7d5fac60aca7c1d8b56063fb8ed0fcd1b92b50f
sachet <sachetwasti61@gmail.com>
2021-07-04 14:42:54 +0530

    [ADD] Initial Code setup.

---
+++ src/main/java/com/sachet/orderreactive/OrderReactiveApplication.java
@@ -0,0 +1,13 @@
+package com.sachet.orderreactive;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrderReactiveApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OrderReactiveApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sachet/orderreactive/OrderReactiveApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sachet.orderreactive;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderReactiveApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
